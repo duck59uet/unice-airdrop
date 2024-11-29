e86ad38f00b8ec28066af560e0fb32fa57624873
duck59uet <ducnv1911@gmail.com>
2024-11-29 22:20:03 +0700

    fix: user statistic

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -57,13 +57,27 @@ export class UserRepository {
 
   async getUserStaking(address: string): Promise<any> {
     return this.repo.query(
-      `WITH user_data AS (select u.wallet as self, COALESCE(sum(ssd.amount), 0) as self_stake, sum(sd.amount) child_staked,
-count(distinct ref.id) as friend from users u left join users ref on u.id = ref."referredBy"
-left join staking_data sd on ref.wallet = sd.wallet
-left join staking_data ssd on u.wallet = ssd.wallet
-group by u.wallet, ref.wallet)
-select self, child_staked, friend, self_stake,  RANK() OVER (ORDER BY self_stake DESC) AS stake_rank from user_data
-where self = $1`,
+      `WITH user_data AS (
+    SELECT
+        u.wallet AS self,
+        COALESCE(SUM(ssd.amount), 0) AS self_stake,
+        COALESCE(SUM(sd.amount), 0) AS child_staked,
+        COUNT(DISTINCT CASE WHEN ref.id IS NOT NULL THEN ref.id END) AS friend
+    FROM users u
+             LEFT JOIN users ref ON u.id = ref."referredBy"
+             LEFT JOIN staking_data sd ON ref.wallet = sd.wallet
+             LEFT JOIN staking_data ssd ON u.wallet = ssd.wallet
+    GROUP BY u.wallet
+)
+SELECT
+    self,
+    child_staked,
+    friend,
+    self_stake,
+    RANK() OVER (ORDER BY self_stake DESC) AS stake_rank
+FROM (
+         SELECT * FROM user_data WHERE self = '${address}'
+     ) filtered_data;`,
       [address],
     );
   }
